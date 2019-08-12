a5e67ccc30736bc42178b35c657be73b6708d8a4
Isaack Okello Brian <isaack@Isaacks-MacBook-Pro.local>
2019-08-12 18:51:01 +0200

    adding managers portal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Row, Col, Divider, Typography } from "antd";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  hashHistory
+} from "react-router-dom";
+import { Breadcrumb, Divider, Typography, Icon } from "antd";
 import ViewsHome from "./views/Home";
 import ViewsTimelog from "./views/Timelog";
 import ViewsProfile from "./views/Profile";
 import ViewsJobs from "./views/Jobs";
+import ViewsManagers from "./views/Managers";
 import ViewsJobsPage from "./views/JobsPage";
 import ViewsOrganizationPage from "./views/OrganizationPage";
 import ViewsTabs from "./views/Tabs";
@@ -12,7 +19,6 @@ import TbHeader from "./Components/TbHeader";
 import TbHeaderMobile from "./Components/TbHeaderMobile";
 import "./App.scss";
 
-const { Title } = Typography;
 class App extends Component {
   render() {
     return (
@@ -28,15 +34,30 @@ class App extends Component {
             <TbHeaderMobile />
           </div>
           <Divider
-            className={"tb-margin-bottom-large"}
+            className={"tb-margin-bottom-medium"}
             style={{ marginTop: "0px" }}
           />
-
+          <div className={"centered"}>
+            <Breadcrumb className={"tb-margin-bottom-large"}>
+              <Breadcrumb.Item href="">
+                <Link to="/">
+                  <Icon type="home" />
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item href="">
+                <Icon type="usergroup-add" />
+                <span>
+                  <Link to="/managers">Managers Portal</Link>
+                </span>
+              </Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
           <Switch>
             <Route path="/" exact component={ViewsHome} />
             <Route path="/timelogs" exact component={ViewsTimelog} />
             <Route path="/profile" exact component={ViewsProfile} />
             <Route path="/jobs" exact component={ViewsJobs} />
+            <Route path="/managers" exact component={ViewsManagers} />
             <Route path="/jobs/:id" exact component={ViewsJobsPage} />
             <Route
               path="/organizations"
